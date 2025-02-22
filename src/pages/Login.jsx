223eae566c43d5c3e5d35534bca4cdc4931d609e
iamabutalha <maabutalhakhalil@gmail.com>
2025-02-22 09:20:39 +0500

    add api and connect admin

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,36 +1,119 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 function Login() {
-  const [currentState, setCurrentState] = useState('Sign Up')
+  const [currentState, setCurrentState] = useState("Login");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const { token, setToken, navigate, backendUrl } = useContext(ShopContext);
 
-  const onSubmitHandler = (e) => {
-    e.preventDefault()
-  }
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      if (currentState === "Sign Up") {
+        const response = await axios.post(backendUrl + "/api/user/register", {
+          name,
+          email,
+          password,
+        });
+        if (response.data.success) {
+          setToken(response.data.token);
+          localStorage.setItem("token", response.data.token);
+        } else {
+          toast.error(response.data.message);
+        }
+
+        console.log(response.data);
+      } else {
+        const response = await axios.post(
+          backendUrl + "/api/user/login",
+          { email, password },
+          { headers: token }
+        );
+
+        if (response.data.success) {
+          setToken(response.data.token);
+          localStorage.setItem("token", response.data.token);
+        } else {
+          toast.error(response.data.message);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      navigate("/");
+    }
+  }, [token]);
 
   return (
-    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
-      <div className='inline-flex items-center gap-2 mb-2 mt-10'>
-          <p className='prata-regular text-3xl '>{currentState}</p>
-          <hr className='border h-[1.5px] w-8 gb-gray-800'/>
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
+    >
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className="prata-regular text-3xl ">{currentState}</p>
+        <hr className="border h-[1.5px] w-8 gb-gray-800" />
       </div>
-      {
-      currentState === 'Sign Up' ? <input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Name'required/>: null
-      }
-      
-      <input type="email" className='w-full px-3 py-2 border border-gray-800' placeholder='Email'required/>
-      <input type="password" className='w-full px-3 py-2 border border-gray-800' placeholder='Password'required/>
+      {currentState === "Sign Up" ? (
+        <input
+          type="text"
+          className="w-full px-3 py-2 border border-gray-800"
+          placeholder="Name"
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      ) : null}
 
-      <div className='w-full flex justify-between text-sm mt-[-8px]'>
-          <p className='cursor-pointer'>Forgot Your Password?</p>
-          {
-            currentState === 'Login'
-             ? <p className='cursor-pointer' onClick={() => setCurrentState('Sign Up')}>Create Account</p>
-             :<p className='cursor-pointer' onClick={() => setCurrentState('Login')}>Login Here</p>
-          }
+      <input
+        type="email"
+        className="w-full px-3 py-2 border border-gray-800"
+        placeholder="Email"
+        required
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        className="w-full px-3 py-2 border border-gray-800"
+        placeholder="Password"
+        required
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <div className="w-full flex justify-between text-sm mt-[-8px]">
+        <p className="cursor-pointer">Forgot Your Password?</p>
+        {currentState === "Login" ? (
+          <p
+            className="cursor-pointer"
+            onClick={() => setCurrentState("Sign Up")}
+          >
+            Create Account
+          </p>
+        ) : (
+          <p
+            className="cursor-pointer"
+            onClick={() => setCurrentState("Login")}
+          >
+            Login Here
+          </p>
+        )}
       </div>
-      <button className='cursor-pointer border border-gray-500 bg-black text-white font-light px-8 py-2 mt-4'>{currentState === 'Sign Up' ? 'Sign Up ': 'Sign In'}</button>
+      <button className="cursor-pointer border border-gray-500 bg-black text-white font-light px-8 py-2 mt-4">
+        {currentState === "Sign Up" ? "Sign Up " : "Sign In"}
+      </button>
     </form>
-  )
+  );
 }
 
-export default Login
+export default Login;
