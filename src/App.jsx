ed15709c8fb3ef9b4b302dc9762366238c87a149
iamabutalha <maabutalhakhalil@gmail.com>
2025-02-22 10:13:07 +0500

    fix error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import "./app.css";
 import Home from "./pages/Home.jsx";
 import Collection from "./pages/Collection.jsx";
 import About from "./pages/About.jsx";
